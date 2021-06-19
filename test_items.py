407480484d0db1d7fdba13ef9bd943ba2bc8a781
Alexander Pavlov <saniok_92@inbox.ru>
2021-06-19 14:23:54 +0300

    Added files for tests

---
+++ test_items.py
@@ -0,0 +1,9 @@
+import time
+link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+
+class TestCatalog():
+    def test_basket_button(self, browser):
+        print("\nstart test...")
+        browser.get(link)
+        browser.find_element_by_css_selector('button.btn-add-to-basket')
+        print("\nend test...")
